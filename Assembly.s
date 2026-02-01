84cc2e1e8a96e06d61b5d23312a5f9a42d26047f
Rikuhhh <mohamed.ben-saoud@oteria.fr>
2026-02-01 19:45:07 +0100

    t3

--- Assembly.s
+++ Assembly.s
@@ -1,14 +1,14 @@
 global main
 main:
-    mov rax, 0x6c6c6568
+    mov rax, 0x0a646c72
     push rax
-    mov rax, 0x646c726f77206f
+    mov rax, 0x6f77206f6c6c6568
     push rax
 
     mov rax, 1
     mov rdi, 1
     mov rsi, rsp
-    mov rdx, 12
+    mov rdx, 11
     syscall
 
     mov rax, 60
